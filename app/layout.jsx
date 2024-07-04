6dd91123cf2b6b40a7e46e1f49a735d2bf9d25ce
Brahim Knioui <brahimknioui@Brahims-MacBook-Air.local>
2024-07-04 04:46:04 +0300

    added styles,fonts and image

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,9 @@
-import { Inter } from "next/font/google";
+import { Rubik } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/Navbar";
-const inter = Inter({ subsets: ["latin"] });
 
+const rubik = Rubik({ subsets: ["latin"] });
+//components
+import Navbar from "./components/Navbar";
 export const metadata = {
   title: "Tickets",
   description: "Tickets for sale",
@@ -11,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={rubik.className}>
       <Navbar/>
         {children}
       </body>
