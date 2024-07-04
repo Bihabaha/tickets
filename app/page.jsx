9737702141fbc373a55481bcfda97494ad03de99
Brahim Knioui <brahimknioui@Brahims-MacBook-Air.local>
2024-07-04 04:29:32 +0300

    navbar components and links

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
    <main>
-    <h1 className="text-center text-primary ">hello</h1>
+    <h1 className="text-center text-primary ">Dashboard</h1>
    </main>
   );
 }
