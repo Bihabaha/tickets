94753fcb9bc6f42fa2b2e26334bf978e4c008291
Brahim Knioui <brahimknioui@Brahims-MacBook-Air.local>
2024-07-04 04:57:47 +0300

    image

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,10 +1,19 @@
 import Link from 'next/link'
 import React from 'react'
 
+import Image from 'next/image'
+import Logo from './moomin3.png'
 function Navbar() {
   return (
     <div>
           <nav>
+           <Image
+           src={Logo}
+           width={60}
+           quality={100}
+           alt='Dojo help desk logo'
+           placeholder='blur'
+           />
           <h1>Dojo helpdesk</h1>
           <Link href="/">Dashboard</Link>
           <Link href="/tickets">Tickets</Link>
